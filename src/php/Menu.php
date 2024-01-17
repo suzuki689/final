1f2442f246959e8e7c4b242f93478bde5f29c8f6
m1kqn <137014023+suzuki689@users.noreply.github.com>
2024-01-17 10:42:21 +0900

    変更

--- src/php/Menu.php
+++ src/php/Menu.php
@@ -3,10 +3,10 @@
         <div class="logo">Adoさん　楽曲</div>
         <nav class="header-menu">
             <ul>
-                <li><a href="Top.php">トップ</li>
-                <li><a href="touroku.php">新規登録</li>
-                <li><a href="delete.php">削除</li>
-                <li><a href="update.php">更新</li>
+                <li><a href="Top.php">トップ</a></li>
+                <li><a href="touroku.php">新規登録</a></li>
+                <li><a href="delete.php">削除</a></li>
+                <li><a href="update.php">更新</a></li>
             </ul>
         </nav>
     </header>
--- src/php/top.php
+++ src/php/top.php
@@ -3,3 +3,36 @@
 <link rel="stylesheet" href="../css/top.css">
 <title>Top</title>
 <?php require 'Menu.php' ;?>
+
+    <!--絞り込み機能-->
+    <div class="narrow-box">
+        <form action="top.php" method="post">
+            <select name="coverFilter" id="coverFilter">
+                <option value="">絞り込まない</option>
+                <option value="0">オリジナル曲</option>
+                <option value="1">カバー曲</option>
+            </select>  
+        <input type="submit" value="絞り込む" id="cover">
+        </form>
+    </div>
+
+    <!--登録楽曲一覧機能-->
+    <?php
+    $pdo = new PDO($connect, USER, PASS);
+    
+    $sql = "SELECT name, time FROM music";
+    $stmt = $pdo->query($sql);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($result as $row){
+        echo '<div class="music-box">';
+        echo '<div class="music-name">';
+        echo $row['name'];
+        echo '</div>';
+        echo '<div class="music-time">';
+        echo $row['time'];
+        echo '</div>';
+        echo '</div>';
+    }
+    $pdo=null;
+    ?>
+<?php require 'footer.php' ;?>
